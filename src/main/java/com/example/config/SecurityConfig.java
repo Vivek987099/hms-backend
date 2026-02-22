712a374ec0ad3cd1c8184e1f97b5d71654d6b791
Vivek987099 <viveksinghfzd8868965223@gmail.com>
2026-02-23 04:41:06 +0530

    configure url

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173","https://*.vercel.app"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173","https://hms-pdj69g6dw-vivek-aryas-projects-158ec312.vercel.app"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers", "path"));
