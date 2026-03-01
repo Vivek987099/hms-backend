ca158d33c426a1ba14c341cba72b2cc4a77381c4
Vivek987099 <viveksinghfzd8868965223@gmail.com>
2026-03-01 22:15:21 +0530

    add root point

--- src/main/java/com/example/tempUser/controller/TempUserController.java
+++ src/main/java/com/example/tempUser/controller/TempUserController.java
@@ -1,17 +1,15 @@
 package com.example.tempUser.controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/temp-user")
+
 public class TempUserController {
 	
-	@GetMapping("/welcome")
+	@GetMapping
 	public ResponseEntity<?> welcome(){
-		return ResponseEntity.ok("controller actice now");
+		return ResponseEntity.ok("HMS backend is runnig");
 	}
 	
 	
